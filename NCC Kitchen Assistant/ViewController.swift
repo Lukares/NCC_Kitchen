fe05fd9fa812a5857b1910b6ae8523d1b99a8741
Reddick <lmreddick@TUCL330010.COS.TCU.EDU>
2017-11-16 12:41:19 -0600

    reset page view

---
+++ NCC Kitchen Assistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NCC Kitchen Assistant
+//
+//  Created by Reddick, Luke on 11/16/17.
+//  Copyright © 2017 Naturally Curly Cook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
