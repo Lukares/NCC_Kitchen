053facc8c3e41c9beeef7e8c752c5e42c1a3a7f9
Reddick <lmreddick@TUCL330010.COS.TCU.EDU>
2017-11-21 13:54:25 -0600

    Initialize MSAL Framework and OAuth Connection

--- NCC Kitchen Assistant/AppDelegate.swift
+++ NCC Kitchen Assistant/AppDelegate.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Naturally Curly Cook. All rights reserved.
 //
 
+import MSAL
 import UIKit
 
 @UIApplicationMain
@@ -39,6 +40,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    // @brief Handles inbound URLs. Checks if the URL matches the redirect URI for a pending AppAuth
+    // authorization request and if so, will look for the code in the response.
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        
+        print("Received callback!")
+        
+        MSALPublicClientApplication.handleMSALResponse(url)
+        
+        return true
     }
 
 
--- NCC Kitchen Assistant/ViewController.swift
+++ NCC Kitchen Assistant/ViewController.swift
@@ -1,25 +1,142 @@
-//
-//  ViewController.swift
-//  NCC Kitchen Assistant
-//
-//  Created by Reddick, Luke on 11/16/17.
-//  Copyright © 2017 Naturally Curly Cook. All rights reserved.
-//
-
 import UIKit
+import MSAL
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, URLSessionDelegate {
+    
+    let kClientID = "2ea9f1f8-d612-4e18-84aa-cf70bfd5b298"
+    let kAuthority = "https://login.microsoftonline.com/common/v2.0"
+    
+    let kGraphURI = "https://graph.microsoft.com/v1.0/me/"
+    let kScopes: [String] = ["https://graph.microsoft.com/user.read"]
+    
+    var accessToken = String()
+    var applicationContext = MSALPublicClientApplication.init()
+    
+    @IBOutlet weak var loggingText: UITextView!
+    @IBOutlet weak var signoutButton: UIButton!
+    
+    // This button will invoke the call to the Microsoft Graph API. It uses the
+    // built in Swift libraries to create a connection.
+    
+    @IBAction func callGraphButton(_ sender: UIButton) {
+        
+        
+        do {
+            
+            // We check to see if we have a current logged in user. If we don't, then we need to sign someone in.
+            // We throw an interactionRequired so that we trigger the interactive signin.
+            
+            if  try self.applicationContext.users().isEmpty {
+                throw NSError.init(domain: "MSALErrorDomain", code: MSALErrorCode.interactionRequired.rawValue, userInfo: nil)
+            } else {
+                
+                // Acquire a token for an existing user silently
+                
+                try self.applicationContext.acquireTokenSilent(forScopes: self.kScopes, user: applicationContext.users().first) { (result, error) in
+                    
+                    if error == nil {
+                        self.accessToken = (result?.accessToken)!
+                        self.loggingText.text = "Refreshing token silently)"
+                        self.loggingText.text = "Refreshed Access token is \(self.accessToken)"
+                        
+                        self.signoutButton.isEnabled = true;
+                        self.getContentWithToken()
+                        
+                    } else {
+                        self.loggingText.text = "Could not acquire token silently: \(error ?? "No error information" as! Error)"
+                        
+                    }
+                }
+            }
+        }  catch let error as NSError {
+            
+            // interactionRequired means we need to ask the user to sign-in. This usually happens
+            // when the user's Refresh Token is expired or if the user has changed their password
+            // among other possible reasons.
+            
+            if error.code == MSALErrorCode.interactionRequired.rawValue {
+                
+                self.applicationContext.acquireToken(forScopes: self.kScopes) { (result, error) in
+                    if error == nil {
+                        self.accessToken = (result?.accessToken)!
+                        self.loggingText.text = "Access token is \(self.accessToken)"
+                        self.signoutButton.isEnabled = true;
+                        self.getContentWithToken()
+                        
+                    } else  {
+                        self.loggingText.text = "Could not acquire token: \(error ?? "No error information" as! Error)"
+                    }
+                }
+                
+            }
+            
+        } catch {
+            
+            // This is the catch all error.
+            
+            self.loggingText.text = "Unable to acquire token. Got error: \(error)"
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        do {
+            // Initialize a MSALPublicClientApplication with a given clientID and authority
+            self.applicationContext = try MSALPublicClientApplication.init(clientId: kClientID, authority: kAuthority)
+        } catch {
+            self.loggingText.text = "Unable to create Application Context. Error: \(error)"
+        }
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        if self.accessToken.isEmpty {
+            signoutButton.isEnabled = false; 
+        }
+    }
+    
+    func getContentWithToken() {
+        
+        let sessionConfig = URLSessionConfiguration.default
+        
+        // Specify the Graph API endpoint
+        let url = URL(string: kGraphURI)
+        var request = URLRequest(url: url!)
+        
+        // Set the Authorization header for the request. We use Bearer tokens, so we specify Bearer + the token we got from the result
+        request.setValue("Bearer \(self.accessToken)", forHTTPHeaderField: "Authorization")
+        let urlSession = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: OperationQueue.main)
+        
+        urlSession.dataTask(with: request) { data, response, error in
+            let result = try? JSONSerialization.jsonObject(with: data!, options: [])
+            if result != nil {
+                
+                self.loggingText.text = result.debugDescription
+            }
+            }.resume()
+    }
+    
+    @IBAction func signoutButton(_ sender: UIButton) {
+        
+        do {
+            
+            // Removes all tokens from the cache for this application for the provided user
+            // first parameter:   The user to remove from the cache
+            
+            try self.applicationContext.remove(self.applicationContext.users().first)
+            self.signoutButton.isEnabled = false;
+            
+        } catch let error {
+            self.loggingText.text = "Received error signing user out: \(error)"
+        }
+    }
 }
 
